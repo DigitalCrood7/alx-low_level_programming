61e1b4875261085add3321a5b71742fb12c3b022
Yabeths Fasil <wildsilence0101@gmail.com>
2022-03-12 05:23:21 -0800

    3rd task - print alphabets;

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -2,21 +2,21 @@
 /**
  * main - Entry point
  *
- * Returnrn: Always 0 (Success/correct)
+ * Return: Always 0 (Success/correct)
  */
 int main(void)
 {
 	char lower_case;
 	char upper_case;
 
+	for (lower_case = 'a'; lower_case <= 'Z'; lower_case++)
+	{
+		putchar(lower_case);
+	}
+
 	for (upper_case = 'A'; upper_case <= 'Z'; upper_case++)
 	{
 		putchar(upper_case);
-	}
-
-	for (lower_case = 'a'; lower_case <= 'z'; lower_case++)
-	{
-		putchar(lower_case);
 	}
 
 	putchar('\n');
