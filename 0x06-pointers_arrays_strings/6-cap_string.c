86577abbf8e300b64447b3ce4ba1fd39f21e8d92
Jabez F <86470839+DigitalCrood7@users.noreply.github.com>
2022-03-24 21:52:37 +0300

    6th task - cap string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,58 +1,29 @@
-#include "main.h
+#include "main.h"
 /**
- * separators - checks and ensure that all string is capitalized
- * @c: character to be checked
- * Return: if separator return 1. Otherwise return 0;
- */
-int separator(char c)
-{
-switch (c)
-{
-case ' ':
-case '\t':
-case '\n':
-case ',':
-case ';':
-case '.':
-case '!':
-case '?':
-case '"':
-case '(':
-case ')':
-case '{':
-case '}':
-return (1);
-
-default:
-return (0);
-}
-
-
-}
-/**
- * cap_string - capitalizes all words of a string
- * @s: string to uppercase
- * Return: returns the modified string
+ * cap_string - capitalize every word of a string
+ *
+ * @s: string to modify
+ *
+ * Return: the resulting string
  */
 char *cap_string(char *s)
 {
-int count, upper;
+	int x = 0, y;
+	char special[13] = {9, 10, 32, 33, 34, 40, 41, 44, 46, 59, 63, 123, 125};
 
-upper = -32;
-
-count = 0;
-
-while (s[count] != '\0')
-{
-if (s[count] >= 'a' && s[count] <= 'z')
-{
-
-
-if (s[count] == *s || separator(s[count - 1]))
-
-s[count] += upper;
-}
-count++;
-}
-return (s);
+	while (*(s + x))
+	{
+		for (y = 0; y < 13; y++)
+		{
+			if (x == 0 && s[x] >= 97 && s[x] <= 122)
+				s[x] -= 32;
+			if (s[x - 1] == special[y])
+			{
+				if ((*(s + x) >= 97) &&  (*(s + x) <= 122))
+					*(s + x) -= 32;
+			}
+		}
+		x++;
+	}
+	return (s);
 }
