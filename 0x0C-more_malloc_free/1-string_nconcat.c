5e80a7cd490e5c912f3a1f7c41b0bd926da1d438
Jabez F <86470839+DigitalCrood7@users.noreply.github.com>
2022-04-08 20:16:44 +0300

    update

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,29 +1,51 @@
 #include "main.h"
-
+#include <stdlib.h>
 /**
- * main - multiplies two positive numbers
- * @argc: n arguments
- * @argv: args
- * Return: int
+ * *string_nconcat - Function that concatenates two strings
+ *
+ * @s1: string 1
+ * @s2: string 2
+ * @n: max bytes of s2
+ *
+ * Return: Return pointer shall point to newly allocated space in memory
  */
-int main(int argc, char *argv[])
+char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-unsigned long mul;
-int i, j;
-	if (argc != 3)
-	{ printf("Error\n");
-	exit(98); }
-	for (i = 1; i < argc; i++)
-	{
-		for (j = 0; argv[i][j] != '\0'; j++)
-		{
-			if (argv[i][j] > 57 || argv[i][j] < 48)
-			{  printf("Error\n");
-			exit(98); }
-		}
+	char *a;
+	unsigned int x, n1 = 0, n2 = 0, i, j;
 
+	if (s1 != NULL)
+	{
+		while (s1[n1] != 0)
+			n1++;
 	}
-	mul = atol(argv[1]) *atol(argv[2]);
-	printf("%lu\n", mul);
-return (0);
+	else
+		s1 = "";
+	if (s2 != NULL)
+	{
+		while (s2[n2] != 0)
+			n2++;
+	}
+	else
+		s2 = "";
+
+	if (n < n2)
+		x = n;
+	else
+		x = n2;
+	a = malloc(sizeof(char) * (n1 + x + 1));
+
+	if (a == NULL)
+		return (NULL);
+	for (i = 0; i < n1; i++)
+	{
+		a[i] = s1[i];
+	}
+	for (j = 0; j < x; j++)
+	{
+		a[i + j] = s2[j];
+	}
+	a[i + j] = 0;
+
+	return (a);
 }
